b7fe66203b20a66824b373fb9dd83b3a552a3c12
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-19 18:37:01 +0530

    Added binarysearch program

--- src/com/yml/main/Main.java
+++ src/com/yml/main/Main.java
@@ -1,10 +1,15 @@
 package com.yml.main;
 
+import java.io.FileNotFoundException;
+
+import com.yml.algorithms.BinarySearch;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Welcome to algorithm programs");
-
+		
+		BinarySearch.run();
 	}
 
 }
